866462c278bf084d805ab21e525a8a8122758960
Rahul Gupta <rahulg.wappnet@gmail.com>
2022-11-30 17:31:54 +0530

    Added comments in index.js of abstract class and edited README

--- abstract class/README.md
+++ abstract class/README.md
@@ -1,5 +1,8 @@
 # Abstract Class in Java Script
 
+Abstract classes can be defined as classes that cannot be instantiated i.e. whose object reference cannot be created and contains within it, one or more abstract methods. An abstract method is a method that can only be declared but has no implementation to it.
+
+
 Actually there is no such keyword called Abstrct in Java Script.
 
 Still to implement the abstract class we have created a class Animal this class can not instantiated but can be extended.We have also defined a function that must be implemented in all classes that exteended from this(Animal) class.
--- abstract class/index.js
+++ abstract class/index.js
@@ -1,10 +1,14 @@
+//Animal class it will act as our abstract class
+
 class Animal {
     constructor(){
+        //if constructor get instantiated it will throw error
         if(this.constructor === Animal){
             throw new Error("Abstract classes can't be instantiated.");
         }
     }
 
+    //if say function get envoked without implementation it will throw an error
     say(){
         throw new Error("Method 'say()' must be implemented.");
     }
@@ -14,14 +18,20 @@ class Animal {
     }
 }
 
+
+//It is child of Animal class
 class Dog extends Animal{
+    //Implementation of say (abstract function)
     say(){
         console.log("Bark");
     }
 }
 
+//Object of Dog class
 var d = new Dog();
 d.eat();;
 d.say();
+
+//Object of Animal class
 var a = new Animal();
 a.say();
